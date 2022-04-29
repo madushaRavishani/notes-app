90e42e6573b6db3a7db769fcafb7d669e4d86eca
madushad <madushad@projectxspace.com>
2022-04-29 07:33:54 +0530

    Add notes-app

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    env: {
+        MONGO_URI: "mongodb+srv://madusha:madusha@cluster0.qb4df.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    }
+}
---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import 'semantic-ui-css/semantic.min.css'
+import Layout from '../components/Layout';
+import '../css/style.css';
+
+function MyApp({ Component, pageProps }) {
+
+    return <Layout><Component {...pageProps} /></Layout>
+}
+export default MyApp
